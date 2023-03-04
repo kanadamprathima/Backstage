54d8e82573be9ad44925424bc4fb9a4b8859efb3
prathima <kanadamprathima@gmail.com>
2023-03-04 18:53:47 +0100

    get all todos&post new todo for user

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
@@ -15,6 +15,59 @@ app.use(bodyParser.json());
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany({ include: { todos: true } });
   res.json(users);
+});
+//endpoint for specific user details
+app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(id) },
+    include: { todos: true },
+  });
+  if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  res.json(user);
+});
+// Get all todos for a user
+app.get("/users/:userId/todos", async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  try {
+    const todos = await prisma.todo.findMany({
+      where: {
+        userId: parseInt(userId),
+      },
+    });
+
+    res.json(todos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+// Create a new todo for a user
+app.post("/users/:userId/todos", async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const { title, description } = req.body;
+
+  try {
+    const todo = await prisma.todo.create({
+      data: {
+        title,
+        description,
+        user: {
+          connect: {
+            id: parseInt(userId),
+          },
+        },
+      },
+    });
+
+    res.json(todo);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.listen(PORT, () => console.log(`server listening on ${PORT}`));
