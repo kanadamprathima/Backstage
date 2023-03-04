d21ceb9d3bfcf11172ac9c610b0579c5bfb34455
prathima <kanadamprathima@gmail.com>
2023-03-04 17:37:30 +0100

    get users endpoint

--- index.ts
+++ index.ts
@@ -9,5 +9,11 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+//endpoint to get all users
+//http :3000/users
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany({ include: { todos: true } });
+  res.json(users);
+});
 
 app.listen(PORT, () => console.log(`server listening on ${PORT}`));
